53dd8bb902b7d4451c28f69bd8a83ba7d766a9e3
Pranamya Kulal <spacedust26@gmail.com>
2024-04-07 13:33:50 +0530

    al lab

--- SEM-4/Algorithms-Lab/lab4-BruteForceII/assignment.c
+++ SEM-4/Algorithms-Lab/lab4-BruteForceII/assignment.c
@@ -3,54 +3,60 @@
 //Output - Lowest cost and job assigmnent
 //Time efficency - O(n!)
 
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define MAX_N 40
+int v, graph[20][20];
+int jobs[20];
+int mincost = 9999;
+int currentcost = 0;
+int permutestore[20];
 
-int cost[MAX_N][MAX_N];
-int assignment[MAX_N];
-int min_assignment[MAX_N];
-int used[MAX_N];
-int min_cost = 999999;
-int opcount = 0;
+//swap function
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-void permute(int n, int index, int total_cost){
-    if (index == n){
-        if (total_cost < min_cost){
-            min_cost = total_cost;
-            for (int i = 0; i < n; i++) {
-                min_assignment[i] = assignment[i];
+//permute function
+void permuteJobs(int jobs[], int start, int end){
+    if(start == end){
+        currentcost = 0;
+        for(int i = 0 ; i < v ; i++){
+            currentcost += graph[i][jobs[i]];
+        }
+        if(currentcost < mincost){
+            mincost = currentcost;
+            for(int i = 0 ; i < v ; i++){
+                permutestore[i] = jobs[i];
             }
         }
-        opcount++;
-        return;
-    }
-    for (int i = 0; i < n; i++) {
-        if (!used[i]) {
-            used[i] = 1;
-            assignment[index] = i;
-            permute(n, index + 1, total_cost + cost[index][i]);
-            used[i] = 0;
+    }else{
+        for(int i = start ; i<= end ; i++){
+            swap(&jobs[start], &jobs[i]);
+            permuteJobs(jobs, start+1, end);
+            swap(&jobs[start], &jobs[i]);
         }
     }
 }
 
-int main() {
-    int n;
-    printf("Enter numbers of jobs to be assigned to the persons ");
-    scanf("%d", &n);
-    printf("Enter the cost matrix (%d x %d):\n", n, n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &cost[i][j]);
+//Main function
+int main(){
+    printf("Enter the total number of jobs available: ");
+    scanf("%d",&v);
+    printf("Enter the cost matrix: ");
+    for(int i = 0 ; i < v ; i++){
+        for(int j = 0 ; j < v ;j++){
+            scanf("%d",&graph[i][j]);
         }
     }
-    permute(n, 0, 0);
-    printf("Minimum cost of assignment: %d\n", min_cost);
-    printf("Assignments:\n");
-    for (int i = 0; i < n; i++) {
-        printf("Person %d -> Job %d\n", i+1, min_assignment[i]+1);
+    for(int i = 0 ; i < v ; i++){
+        jobs[i] = i;
     }
-    printf("Opcount = %d ",opcount);
-    return 0;
+    permuteJobs(jobs, 0 , v-1);
+    printf("\nMinimum cost of assigning jobs : %d\n", mincost);
+    for(int i = 0 ; i < v ; i++){
+        printf("Person %d - Job %d\n", i+1, permutestore[i] + 1);
+    }
 }
