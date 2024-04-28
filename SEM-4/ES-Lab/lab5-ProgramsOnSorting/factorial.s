d2c7caa4e083e7eb79e7f0b815f5055f67f5ae57
Pranamya Kulal <spacedust26@gmail.com>
2024-04-28 12:27:40 +0530

    lab 5 es

---
+++ SEM-4/ES-Lab/lab5-ProgramsOnSorting/factorial.s
@@ -0,0 +1,33 @@
+;To find the factorial of a given number using recusrion
+	AREA RESET, DATA, READONLY
+	EXPORT __Vectors
+__Vectors
+	DCD 0X10001000
+	DCD Reset_Handler
+	ALIGN
+	AREA mycode, CODE, READONLY
+	ENTRY
+	EXPORT Reset_Handler
+Reset_Handler
+	LDR R0, =SRC
+	LDR R1, =DST
+	LDR R2, [R0]
+	BL FACT
+	STR R2, [R1]
+STOP B STOP
+FACT PUSH{R3,LR}
+	MOV R3, R2
+	CMP R2, #0
+	BNE DOWN
+	MOV R2, #1
+	B DOWNL
+DOWN SUB R2, #1
+	BL FACT
+	MOV R4, R3
+	MUL R2, R4
+DOWNL POP{R3,LR}
+	BX LR
+SRC DCD 5
+	AREA mydata, DATA, READWRITE
+DST DCD 0
+	END
