53f68bfe08bb5a499b11a272c7328155d72e4f60
Pranamya Kulal <144557094+spacedust26@users.noreply.github.com>
2024-10-26 17:10:13 +0000

    os lab 2 part 1 codes

---
+++ SEM-5/OS-Lab/Lab2-ShellScripting1/Lab2Part1/lab2q1.sh
@@ -0,0 +1,5 @@
+# .lab2q1.sh
+
+echo $HOME, $PATH
+echo $MAIL
+echo $USER, $SHELL, $TERM
---
+++ SEM-5/OS-Lab/Lab2-ShellScripting1/Lab2Part1/lab2q2.sh
@@ -0,0 +1,10 @@
+# .lab2q2.sh 
+
+firstname=Pranamya
+lastname=Kulal
+echo $firstname $lastname
+export lastname
+sh
+echo $firstname $lastname
+# ctrl + d to terminate child shell
+echo $firstname $lastname
